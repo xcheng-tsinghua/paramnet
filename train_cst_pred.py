0d8974be2d942dc3b4cb2c871f626785648af49c
xcheng-tsinghua <786682191@qq.com>
2025-01-24 23:54:38 +0800

    change

--- train_cst_pred.py
+++ train_cst_pred.py
@@ -33,10 +33,10 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode') # 是否使用CPU
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device') # 指定的GPU设备
-    parser.add_argument('--batch_size', type=int, default=16, help='batch size in training') # batch_size
+    parser.add_argument('--bs', type=int, default=16, help='batch size in training') # batch_size
     parser.add_argument('--model', default='pointnet2_cls_ssg', help='model name [default: pointnet_cls]') # 已训练好的分类模型
     parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40') # 指定训练集 ModelNet10/40
-    parser.add_argument('--epoch', default=1000, type=int, help='number of epoch in training') # 训练的epoch数
+    parser.add_argument('--epoch', default=10, type=int, help='number of epoch in training') # 训练的epoch数
     parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training') # 学习率
     parser.add_argument('--num_point', type=int, default=2000, help='Point Number') # 点数量
     parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training') # 优化器
@@ -45,9 +45,11 @@ def parse_args():
     parser.add_argument('--workers', type=int, default=10, help='dataloader workers')
     parser.add_argument('--save_str', type=str, default='parsenet', help='dataloader workers')
 
+    parser.add_argument('--abc_pack', type=int, default=1, help='dataloader workers')
+
     parser.add_argument('--local', default='True', choices=['True', 'False'], type=str, help='---')
-    parser.add_argument('--root_sever', type=str, default=r'/root/my_data/data_set/STEPMillion/STEPMillion_pack1', help='root of dataset')
-    parser.add_argument('--root_local', type=str, default=r'D:\document\DeepLearning\DataSet\STEPMillion\STEPMillion_1\STEPMillion_pack1', help='root of dataset')
+    parser.add_argument('--root_sever', type=str, default=r'/root/my_data/data_set/STEPMillion/STEPMillion_pack{pack_idx}', help='root of dataset')
+    parser.add_argument('--root_local', type=str, default=r'D:\document\DeepLearning\DataSet\STEPMillion\STEPMillion_{pack_idx}\STEPMillion_pack{pack_idx}', help='root of dataset')
 
     # Parametric20000
     # sever: r'/root/my_data/data_set/STEP20000_Hammersley_2000'
@@ -91,9 +93,12 @@ def main(args):
     else:
         data_root = args.root_sever
 
+    abc_pack = args.abc_pack
+    data_root = str(data_root).replace('{pack_idx}', str(abc_pack))
+
     # train_dataset = MCBDataLoader(root=data_root, npoints=args.num_point, data_augmentation=True, is_train=True, is_back_addattr=True)
     train_dataset = STEPMillionDataLoader(root=data_root, npoints=args.num_point, data_augmentation=True)
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=int(args.workers))  # , drop_last=True
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.bs, shuffle=True, num_workers=int(args.workers))  # , drop_last=True
 
     '''MODEL LOADING'''
     predictor = cst_pcd(n_points_all=args.num_point, n_metatype=args.n_metatype).cuda()
