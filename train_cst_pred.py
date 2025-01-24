1d8b9b26daa3c1cb8e9ff5dc8fa7ecfb5608d470
xcheng-tsinghua <786682191@qq.com>
2025-01-24 23:56:36 +0800

    change

--- train_cst_pred.py
+++ train_cst_pred.py
@@ -33,10 +33,10 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode') # 是否使用CPU
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device') # 指定的GPU设备
-    parser.add_argument('--bs', type=int, default=16, help='batch size in training') # batch_size
+    parser.add_argument('--bs', type=int, default=18, help='batch size in training') # batch_size
     parser.add_argument('--model', default='pointnet2_cls_ssg', help='model name [default: pointnet_cls]') # 已训练好的分类模型
     parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40') # 指定训练集 ModelNet10/40
-    parser.add_argument('--epoch', default=10, type=int, help='number of epoch in training') # 训练的epoch数
+    parser.add_argument('--epoch', default=20, type=int, help='number of epoch in training') # 训练的epoch数
     parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training') # 学习率
     parser.add_argument('--num_point', type=int, default=2000, help='Point Number') # 点数量
     parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training') # 优化器
@@ -47,7 +47,7 @@ def parse_args():
 
     parser.add_argument('--abc_pack', type=int, default=1, help='dataloader workers')
 
-    parser.add_argument('--local', default='True', choices=['True', 'False'], type=str, help='---')
+    parser.add_argument('--local', default='False', choices=['True', 'False'], type=str, help='---')
     parser.add_argument('--root_sever', type=str, default=r'/root/my_data/data_set/STEPMillion/STEPMillion_pack{pack_idx}', help='root of dataset')
     parser.add_argument('--root_local', type=str, default=r'D:\document\DeepLearning\DataSet\STEPMillion\STEPMillion_{pack_idx}\STEPMillion_pack{pack_idx}', help='root of dataset')
 
