e10af93984db8bc068d973e2510408dcab492039
xcheng-tsinghua <786682191@qq.com>
2025-05-15 15:15:40 +0800

    change

--- train_cls_with_cst.py
+++ train_cls_with_cst.py
@@ -58,7 +58,7 @@ def parse_args():
     parser.add_argument('--save_str', type=str, default='ca_final', help='---')
     parser.add_argument('--rotate', default=0, type=int, help='---')
     parser.add_argument('--cst_pcd', type=str, default='cst_pcd_abc25t.pth', help='---')
-    parser.add_argument('--model', type=str, default='PointNet2', choices=['GCN3D', 'DGCNN', 'PointNet', 'PointNet2', 'CstNet'], help='model used for cls')
+    parser.add_argument('--model', type=str, default='CstNet', choices=['GCN3D', 'DGCNN', 'PointNet', 'PointNet2', 'CstNet'], help='model used for cls')
 
     parser.add_argument('--local', default='False', choices=['True', 'False'], type=str, help='---')
     parser.add_argument('--root_sever', type=str,
